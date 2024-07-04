862a4acb1a65a404a74f81b25ea3b010db622c5e
GreatSoshiant <61604245+GreatSoshiant@users.noreply.github.com>
2024-07-03 23:31:34 -0700

    simple contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,34 @@
-//!
-//! Stylus Hello World
-//!
-//! The following contract implements the Counter example from Foundry.
-//!
-//! ```
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
-//!
-//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
-//! To do this, run `cargo stylus export-abi`.
-//!
-//! Note: this code is a template-only and has not been audited.
-//!
-
-// Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
-/// Use an efficient WASM allocator.
 #[global_allocator]
 static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
-/// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*};
+use alloy_primitives::U256;
+use stylus_sdk::{ alloy_primitives::Address, call::Call, prelude::*};
 
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct Counter {
-        uint256 number;
+// Interfaces for the Art contract and the ERC20 contract
+sol_interface! {
+    interface TargetContract {
+        function callBack() external returns(string);
     }
 }
 
-/// Declare that `Counter` is a contract with the following external methods.
+
+#[solidity_storage]
+#[entrypoint]
+pub struct SingleCall;
+
 #[external]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
-        self.number.get()
+impl SingleCall {
+    pub fn execute(&mut self, target: Address) -> String {
+		let target_contract = TargetContract::new(target);
+		let config = Call::new_in(self);
+		
+        let result = target_contract.call_back(config);
+        result.unwrap()
     }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
-        self.number.set(new_number);
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
-        self.number.set(new_number * self.number.get());
-    }
-
-    /// Sets a number in storage to a user-specified value.
-    pub fn sub_number(&mut self, new_number: U256) {
-        self.number.set(new_number + self.number.get());
-    }
-
-    /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
-        let number = self.number.get();
-        self.set_number(number + U256::from(1));
+    pub fn check(&self, number: U256) -> Result<U256, Vec<u8>> {
+        Ok(number.into())
     }
 }
